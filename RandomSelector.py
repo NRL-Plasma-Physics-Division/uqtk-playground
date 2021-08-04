9231e5ef52d2083e441cc2383d8a9d71272eec81
Gareth Morgan <gtmorgan2003@gmail.com>
2021-08-04 17:11:57 -0400

    Created first pass at gaussian distribution selector

---
+++ RandomSelector.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+mu = 0.0
+sigma = 0.1
+selection = np.random.normal(mu, sigma, 1000)
+
+count, bins, ignored = plt.hist(selection, 30, density=True)
+plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) *
+               np.exp( - (bins - mu)**2 / (2 * sigma**2) ),
+         linewidth=2, color='r')
+plt.show()
+
