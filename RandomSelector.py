a42096bbbbe6ffba746eee033fd4d4c460539858
Gareth Morgan <gtmorgan2003@gmail.com>
2021-08-04 20:37:46 -0400

    Improved the plot. Aesthetic changes.

--- RandomSelector.py
+++ RandomSelector.py
@@ -8,6 +8,9 @@ selection = np.random.normal(mu, sigma, 1000)
 count, bins, ignored = plt.hist(selection, 30, density=True)
 plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) *
                np.exp( - (bins - mu)**2 / (2 * sigma**2) ),
-         linewidth=2, color='r')
+         linewidth=2, color='y')
+plt.title("Gaussian Distribution")
+plt.xlabel("Range")
+plt.ylabel("Frequency")
 plt.show()
 
